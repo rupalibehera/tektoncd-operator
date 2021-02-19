d81b0e7c9654bfd447afdb90ac2dc9c9bc24ddfa
Rupali Behera <rbehera@redhat.com>
2021-02-19 13:14:45 +0100

    Switching master to main, adding OPENSHIFT_REMOTE to test changes locally, this script is fixed to merge upstream changes to midstream. This is will resolve https://issues.redhat.com/browse/SRVKP-1338

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -3,27 +3,28 @@
 # Synchs the release-next branch to master and then triggers CI
 # Usage: update-to-head.sh
 
-set -e
+set -ex
 REPO_NAME=`basename $(git rev-parse --show-toplevel)`
+OPENSHIFT_REMOTE=${OPENSHIFT_REMOTE:openshift}
 
-# Reset release-next to upstream/master.
-git fetch upstream master
-git checkout upstream/master -B release-next
+# Reset release-next to upstream/main.
+git fetch upstream main
+git checkout upstream/main --no-track -B release-next
 
 # Update openshift's master and take all needed files from there.
-git fetch openshift master
-git checkout openshift/master openshift OWNERS_ALIASES OWNERS 
+git fetch ${OPENSHIFT_REMOTE} master
+git checkout FETCH_HEAD openshift OWNERS_ALIASES OWNERS
 git add openshift OWNERS_ALIASES OWNERS 
 git commit -m ":open_file_folder: Update openshift specific files."
 
-git push -f openshift release-next
+git push -f ${OPENSHIFT_REMOTE} release-next
 
 # Trigger CI
 git checkout release-next -B release-next-ci
 date > ci
 git add ci
 git commit -m ":robot: Triggering CI on branch 'release-next' after synching to upstream/master"
-git push -f openshift release-next-ci
+git push -f ${OPENSHIFT_REMOTE} release-next-ci
 
 if hash hub 2>/dev/null; then
    hub pull-request --no-edit -l "kind/sync-fork-to-upstream" -b openshift/${REPO_NAME}:release-next -h openshift/${REPO_NAME}:release-next-ci
